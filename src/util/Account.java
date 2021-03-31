8a93b98b39097d3a64c9abc71bc19c690f2cd8fe
Akshith Ramadugu <ak.ramadugu@gmail.com>
2021-03-31 13:12:10 -0400

    Created an account object to handle user account details that will be pushed into the session

---
+++ src/util/Account.java
@@ -0,0 +1,68 @@
+package util;
+
+public class Account {
+    int accountNumber;
+    String firstName;
+    String lastName;
+    String username;
+    String password;
+    String email;
+    int accessLevel;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(int accessLevel) {
+        this.accessLevel = accessLevel;
+    }
+
+}
